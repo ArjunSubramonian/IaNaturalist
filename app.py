c2f488e4e4a0d09c2809a786918e5749493eeec8
ArjunSubramonian <arjun.subramonian@gmail.com>
2020-07-18 16:53:27 -0700

    object detection

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from io import BytesIO
 from flask import Flask, request, render_template
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import base64
 
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
@@ -30,29 +30,26 @@ def index():
         image1 = request.files['image']
         image2 = image1
         client = ComputerVisionClient(COGSVCS_CLIENTURL, CognitiveServicesCredentials(COGSVCS_KEY))
-        message = ''
-        try:
-            result = client.describe_image_in_stream(image1)
+        # try:
+        #     result = client.describe_image_in_stream(image1)
+        #     message = result.captions[0].text
         # except ComputerVisionErrorException as e:
         #     message = str(e.response.text)
-        except Exception as e:
-            message += repr(e)
-        try:
-            message = result.captions[0].text
-        except Exception as e:
-            message += repr(e)
 
         try:
             result = client.detect_objects_in_stream(image2)
+            message = ''
         except ComputerVisionErrorException as e:
             message += str(e.response.text)
 
         img = Image.open(image2)
+        font = ImageFont.truetype("sans-serif.ttf", 16)
         dctx = ImageDraw.Draw(img)  # create drawing context
         for detection in result.objects:
             w, h = detection.rectangle.w, detection.rectangle.h
             bbox = [(detection.rectangle.x, detection.rectangle.y), (w + detection.rectangle.x, h + detection.rectangle.y)]
-            dctx.rectangle(bbox, outline="blue")
+            dctx.rectangle(bbox, outline="red")
+            dctx.text((detection.rectangle.x, detection.rectangle.y), detection.object_property, (255,0,0), font=font)
         del dctx  # destroy drawing context
         
         output = BytesIO()
